b21846d30d72e774aa391ca949c02304430517c1
qixiao <wd8622088@foxmail.com>
2017-05-18 10:48:07 +0800

    update qx_frame

--- 10-code/QX_Frame.App.WebApi/WebApiControllerBase.cs
+++ 10-code/QX_Frame.App.WebApi/WebApiControllerBase.cs
@@ -1,7 +1,38 @@
-﻿namespace QX_Frame.App.WebApi
+﻿using Newtonsoft.Json.Linq;
+using QX_Frame.Helper_DG;
+using QX_Frame.Helper_DG.Configs;
+using QX_Frame.Helper_DG.Extends;
+
+namespace QX_Frame.App.WebApi
 {
     public abstract class WebApiControllerBase: WcfService
     {
-        
+        protected static string GetLB_XXX(int LB_Code)
+        {
+            if (string.IsNullOrEmpty(QX_Frame_Helper_DG_Config.International_ConfigFileLocation))
+            {
+                throw new Exception_DG("QX_Frame_Helper_DG_Config.International_ConfigFileLocation must be provide correctly ! -- QX_Frame.Helper_DG.Extends.Exception_DG line:18");
+            }
+            JObject jobject = File_Helper_DG.Json_GetJObjectFromJsonFile(QX_Frame_Helper_DG_Config.International_ConfigFileLocation);//get json configuration file
+            return jobject[QX_Frame_Helper_DG_Config.International_Language][$"LB_{LB_Code}"].ToString();
+        }
+        protected static string GetMSG_XXX(int MSG_Code)
+        {
+            if (string.IsNullOrEmpty(QX_Frame_Helper_DG_Config.International_ConfigFileLocation))
+            {
+                throw new Exception_DG("QX_Frame_Helper_DG_Config.International_ConfigFileLocation must be provide correctly ! -- QX_Frame.Helper_DG.Extends.Exception_DG line:18");
+            }
+            JObject jobject = File_Helper_DG.Json_GetJObjectFromJsonFile(QX_Frame_Helper_DG_Config.International_ConfigFileLocation);//get json configuration file
+            return jobject[QX_Frame_Helper_DG_Config.International_Language][$"MSG_{MSG_Code}"].ToString();
+        }
+        protected static string GetERROR_XXX(int ERROR_Code)
+        {
+            if (string.IsNullOrEmpty(QX_Frame_Helper_DG_Config.International_ConfigFileLocation))
+            {
+                throw new Exception_DG("QX_Frame_Helper_DG_Config.International_ConfigFileLocation must be provide correctly ! -- QX_Frame.Helper_DG.Extends.Exception_DG line:18");
+            }
+            JObject jobject = File_Helper_DG.Json_GetJObjectFromJsonFile(QX_Frame_Helper_DG_Config.International_ConfigFileLocation);//get json configuration file
+            return jobject[QX_Frame_Helper_DG_Config.International_Language][$"ERROR_{ERROR_Code}"].ToString();
+        }
     }
 }
---
+++ 10-code/QX_Frame.WebApi/Helpers/Return_Helper.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * author:qixiao
+ * create:2017-5-17 14:32:16
+ * */
+namespace QX_Frame.WebApi.Helpers
+{
+    public class Return_Helper
+    {
+        /// <summary>
+        /// return success result
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="data"></param>
+        /// <param name="dataCount"></param>
+        /// <param name="httpCode"></param>
+        /// <returns></returns>
+        public static object Success(string msg,dynamic data=null,int dataCount=0, System.Net.HttpStatusCode httpCode=System.Net.HttpStatusCode.OK)
+        {
+            return Helper_DG.Return_Helper_DG.Success_Msg_Data_DCount_HttpCode(msg, data, dataCount, httpCode);
+        }
+        /// <summary>
+        /// return faild result
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="errorCode"></param>
+        /// <param name="errorLevel"></param>
+        /// <param name="httpCode"></param>
+        /// <returns></returns>
+        public static object Faild(string msg, int errorCode = 0, int errorLevel = 0, System.Net.HttpStatusCode httpCode = System.Net.HttpStatusCode.InternalServerError)
+        {
+            return Helper_DG.Return_Helper_DG.Error_Msg_Ecode_Elevel_HttpCode(msg, errorCode, errorLevel, httpCode);
+        }
+    }
+}
